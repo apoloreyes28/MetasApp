6ffe5422535c08e59a7b0d8d27f9e505b9424209
apoloreyes28 <reyesapolinar31@gmail.com>
2024-02-05 14:57:33 -0700

    crear componentes principales

---
+++ src/componentes/compartidos/Pie.js
@@ -0,0 +1,9 @@
+function Pie() {
+    return (
+      <footer>
+        <small>Desarrollado y Diseñado por Osvaldo Reyes. Todos los Derechos Reservados. &copy;2030</small>
+      </footer>
+    );
+  }
+  
+  export default Pie;
